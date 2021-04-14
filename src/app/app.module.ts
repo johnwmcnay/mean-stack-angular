c16d8c320c4d0a79b19198331e6792dd1685e43f
johnwmcnay <john.w.mcnay@gmail.com>
2021-04-14 14:21:05 -0500

    added interface and two-way input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
